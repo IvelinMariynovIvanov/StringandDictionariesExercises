4d9125c0d0cd8372eb928cae224247e354570537
IvelinMariyanovIvanov <i.ivanov_1987@abv.bg>
2017-02-19 23:03:35 -0800

    a

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringandDictionariesExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string input = Console.ReadLine();
+            string arr = Console.ReadLine();
+
+            for (int i = arr.Length-1; i >= 0; i--)
+            {
+                Console.Write(arr[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+
+
+
+}
